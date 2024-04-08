7b79a4e01fc61d65ed8877e1dcf5046264fce8a4
Luckaura <116108567+Luckaura@users.noreply.github.com>
2024-04-08 21:04:19 +0300

    Adding more input validation

--- Services/ValidationService.cs
+++ Services/ValidationService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 namespace Aimidge.Services
 {
@@ -14,13 +15,23 @@ namespace Aimidge.Services
         {
             string[] prompt_parts = prompt.Split(' ');
             string[] badWords = File.ReadAllLines("Profanity.txt");
-            foreach (string word in prompt_parts)
-            {
+			string pattern = @"\b\p{L}*?(\p{L})\1{2,}\p{L}*\b|(?=.*[A-Za-z])(?=.*\d)";
+			if (string.IsNullOrWhiteSpace(prompt)) { return prompt; }
+
+			foreach (string word in prompt_parts)
+			{
+				if (Regex.Match(word, pattern).Success)
+					return word;
+			}
+
+
+			foreach (string word in prompt_parts)
+                {
                 if (badWords.Contains(word.ToLower()))
                 {
                     return word;
                 }
-            }
+                }
             return "ok";
         }
     }
